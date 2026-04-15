a8be0f5a7fa460686d6a2c1e3884b494cc1a7807
Douglas Alves <douglas.antonio@academico.ifpb.edu.br>
2026-04-15 12:02:43 -0300

    Add files via upload

---
+++ teste.py
@@ -0,0 +1,5 @@
+import streamlit as st
+
+st.image("WhatsApp Image 2026-04-15 at 11.30.50 (1).png")
+st.write("Site Evellen Vitoria")
+st.link_button("Acessar", "https://sites.google.com/d/1FCkP-l3S7tFGgDnYWZZomDJ5H6bPN7Ya/p/1ysHkLke1PpovZ_gso2t-AZQ2m02rQbTk/edit")
