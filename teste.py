3eafa6df8b482a543a0065ae9d4e4d36a80a81cd
Douglas Alves <douglas.antonio@academico.ifpb.edu.br>
2026-04-15 12:07:38 -0300

    Update teste.py

--- teste.py
+++ teste.py
@@ -1,5 +1,5 @@
 import streamlit as st
 
-st.image("harrypotter.jpeg")
+st.image("harrypotter.jpg")
 st.write("Site Douglas")
 st.link_button("Acessar", "https://sites.google.com/academico.ifpb.edu.br/douglas-antonio-info/in%C3%ADcio")
