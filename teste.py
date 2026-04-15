face5d257a99109c093daa436d4bef544bf35424
Douglas Alves <douglas.antonio@academico.ifpb.edu.br>
2026-04-15 12:04:07 -0300

    Update image and site information in teste.py

--- teste.py
+++ teste.py
@@ -1,5 +1,5 @@
 import streamlit as st
 
-st.image("WhatsApp Image 2026-04-15 at 11.30.50 (1).png")
-st.write("Site Evellen Vitoria")
-st.link_button("Acessar", "https://sites.google.com/d/1FCkP-l3S7tFGgDnYWZZomDJ5H6bPN7Ya/p/1ysHkLke1PpovZ_gso2t-AZQ2m02rQbTk/edit")
+st.image("harrypotter.jpeg")
+st.write("Site Douglas")
+st.link_button("Acessar", "https://sites.google.com/academico.ifpb.edu.br/douglas-antonio-info/in%C3%ADcio")
